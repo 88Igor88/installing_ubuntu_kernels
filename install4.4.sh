0e93c0ffe0fdc1fe0076dce48a885ee7873a1058
88Igor88 <108658382+88Igor88@users.noreply.github.com>
2024-04-05 18:48:01 +0300

    Update install4.4.sh

--- install4.4.sh
+++ install4.4.sh
@@ -5,7 +5,11 @@ wget https://kernel.ubuntu.com/mainline/v4.4.40/linux-headers-4.4.40-040440-gene
 wget https://kernel.ubuntu.com/mainline/v4.4.40/linux-headers-4.4.40-040440-lowlatency_4.4.40-040440.201701060808_amd64.deb
 wget https://kernel.ubuntu.com/mainline/v4.4.40/linux-image-4.4.40-040440-generic_4.4.40-040440.201701060808_amd64.deb
 wget https://kernel.ubuntu.com/mainline/v4.4.40/linux-image-4.4.40-040440-lowlatency_4.4.40-040440.201701060808_amd64.deb
-apt-get install libssl1.0.0
+apt-get install software-properties-common
+sudo apt-add-repository -y ppa:rael-gc/rvm 
+sudo apt-get update 
+sudo apt-get install rvm 
+sudo apt install libssl1.0-dev
 dpkg -i linux-headers-4.4.40-040440_4.4.40-040440.201701060808_all.deb
 dpkg -i linux-headers-4.4.40-040440-generic_4.4.40-040440.201701060808_amd64.deb
 dpkg -i linux-headers-4.4.40-040440-lowlatency_4.4.40-040440.201701060808_amd64.deb
