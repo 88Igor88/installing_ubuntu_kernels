70b1177de974228fba8b6e14d7a56e3937633cf1
88Igor88 <108658382+88Igor88@users.noreply.github.com>
2024-04-05 18:15:44 +0300

    Update install4.sh
    Fixed an error installing some packages

--- install4.sh
+++ install4.sh
@@ -5,7 +5,7 @@ wget https://kernel.ubuntu.com/mainline/v4.4.40/linux-headers-4.4.40-040440-gene
 wget https://kernel.ubuntu.com/mainline/v4.4.40/linux-headers-4.4.40-040440-lowlatency_4.4.40-040440.201701060808_amd64.deb
 wget https://kernel.ubuntu.com/mainline/v4.4.40/linux-image-4.4.40-040440-generic_4.4.40-040440.201701060808_amd64.deb
 wget https://kernel.ubuntu.com/mainline/v4.4.40/linux-image-4.4.40-040440-lowlatency_4.4.40-040440.201701060808_amd64.deb
-
+apt-get install libssl1.0.0
 dpkg -i linux-headers-4.4.40-040440_4.4.40-040440.201701060808_all.deb
 dpkg -i linux-headers-4.4.40-040440-generic_4.4.40-040440.201701060808_amd64.deb
 dpkg -i linux-headers-4.4.40-040440-lowlatency_4.4.40-040440.201701060808_amd64.deb
