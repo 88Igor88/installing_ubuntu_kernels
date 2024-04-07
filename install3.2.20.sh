494b2277de1ee71078db517a4d77cc00bdac25e4
88Igor88 <108658382+88Igor88@users.noreply.github.com>
2024-04-07 15:29:49 +0300

    add script for 3.2.20 kernel

---
+++ install3.2.20.sh
@@ -0,0 +1,7 @@
+mkdir deb3.2.20
+cd deb3.2.20
+wget https://kernel.ubuntu.com/mainline/v3.2.20-precise/linux-headers-3.2.20-030220_3.2.20-030220.201206102035_all.deb
+wget https://kernel.ubuntu.com/mainline/v3.2.20-precise/linux-image-3.2.20-030220-generic_3.2.20-030220.201206102035_amd64.deb
+
+dpkg -i linux-headers-3.2.20-030220_3.2.20-030220.201206102035_all.deb 
+dpkg -i linux-image-3.2.20-030220-generic_3.2.20-030220.201206102035_amd64.deb
